c0c429e847f787eefe81e1baa55a49355bc5e718
alexsockz <valessiobg@gmail.com>
2026-02-28 02:10:23 +0100

    basically i need to add all the possible inputs as security, TODO fix initialize

--- go_dcgp.sh
+++ go_dcgp.sh
@@ -19,5 +19,5 @@ export OMP_PROC_BIND=close
 if [[ ${TOTAL_TASKS} -eq 1 ]]; then
     ${EXEC} -n ${N_STEPS} -x ${GRID_SIZE_X} -y ${GRID_SIZE_Y} -p 1 -o 0
 else
-    mpirun -np ${TOTAL_TASKS} ${EXEC} -n ${N_STEPS} -x ${GRID_SIZE_X} -y ${GRID_SIZE_Y} -p 1 -o 0 -e 10 -E 10
+    mpirun -np ${TOTAL_TASKS} ${EXEC} -n ${N_STEPS} -x ${GRID_SIZE_X} -y ${GRID_SIZE_Y} -p 1 -o 0 -e 10 -E 2
 fi
